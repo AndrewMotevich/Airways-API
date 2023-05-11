bd06b2cbd26ede34a52ddc327a3e63b5af44c0dd
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-11 22:14:17 +0300

    fix: fix cors options

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,8 @@ export const Secret = {
 export const Cors = {
   origin: [
       'http://localhost:8080',
+      'http://localhost:4200',
+      'http://127.0.0.1:4200',
       'http://127.0.0.1:8080',
       'http://127.0.0.1:5500',
       'https://podcast-deploy.vercel.app',
