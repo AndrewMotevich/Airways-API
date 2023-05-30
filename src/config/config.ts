ddf880c46793aa052b32857973d441434948562a
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-30 13:22:15 +0300

    feat: add new cors for develop

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,20 @@
 export const Secret = {
-  accessSecret: 'SECRET_ACCESS_KEY',
-  refreshSecret: 'SECRET_REFRESH_KEY', 
+    accessSecret: 'SECRET_ACCESS_KEY',
+    refreshSecret: 'SECRET_REFRESH_KEY',
 };
 
 export const Cors = {
-  origin: [
-      'http://localhost:8080',
-      'http://localhost:4200',
-      'http://127.0.0.1:4200',
-      'http://127.0.0.1:8080',
-      'http://127.0.0.1:5500',
-      'https://podcast-deploy.vercel.app',
-      'https://aaairways.netlify.app'
-  ],
-  methods: 'GET,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: ['X-admin-pass', 'X-hash-pass', 'Library', 'Content-Type', 'Authorization'],
-  credentials: true,
+    origin: [
+        'http://localhost:8080',
+        'http://localhost:4200',
+        'http://127.0.0.1:4200',
+        'http://127.0.0.1:8080',
+        'http://127.0.0.1:5500',
+        'https://podcast-deploy.vercel.app',
+        'https://aaairways.netlify.app',
+        'https://airways-coral.vercel.app',
+    ],
+    methods: 'GET,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['X-admin-pass', 'X-hash-pass', 'Library', 'Content-Type', 'Authorization'],
+    credentials: true,
 };
